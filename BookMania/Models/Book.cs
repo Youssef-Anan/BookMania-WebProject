1fc42fc593cf47f80a8794560ee0e524bb87acb1
Youssef-Anan <youssef.anan01@gmail.com>
2023-12-11 23:18:10 +0200

    Add project files.

---
+++ BookMania/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMania.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author_Name { get; set; }
+        public string Type { get; set; }
+        public string Genre { get; set; }
+        public float Price { get; set; }
+        public float Rating { get; set; }
+    }
+}
